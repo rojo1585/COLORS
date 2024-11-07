33737e6a0859fd910398e2080be60ba3668ff3f9
Jorge Miguel Mendoza Correa <jorge.mendoza@bdcloud.onmicrosoft.com>
2024-11-07 12:22:16 -0600

    Add Literals

---
+++ Models/Literals.cs
@@ -0,0 +1,25 @@
+﻿namespace COLORS.Models;
+
+public static class Literals
+{
+    public enum Color
+    {
+        Red,
+        Blue
+    }
+
+    private static Dictionary<Color, string> colorsDescriptions = new()
+    {
+        { Color.Red , "RED" }
+    };
+
+    public static Dictionary<Color, string> GetColorsDescriptions()
+    {
+        return colorsDescriptions;
+    }
+    public static class ErrorMessages
+    {
+        public static string NotCommandFound { get; set; } = "Not Color Command exist";
+    }
+
+}
