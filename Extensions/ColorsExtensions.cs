dbe408d6cfc231405552956ddf736e208777c80b
Jorge Miguel Mendoza Correa <jorge.mendoza@bdcloud.onmicrosoft.com>
2024-11-08 12:32:27 -0600

    Add Extension methods for configure app

---
+++ Extensions/ColorsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using COLORS.Factory;
+using COLORS.Modules;
+using COLORS.Modules.Interface;
+using COLORS.UI;
+using COLORS.UI.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace COLORS.Extensions;
+public static class ColorsExtensions
+{
+    public static IHostBuilder AddServices(this IHostBuilder host) =>
+   host.ConfigureServices((context, services) =>
+   {
+       services.AddScoped<IInterpeter, ColorInterpreter>();
+       services.AddScoped<IColorCommandFactory, ColorCommandFactory>();
+       services.AddScoped<IUI, ColorProgramUI>();
+       services.AddSingleton<App>();
+   });
+}
