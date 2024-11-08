ebe9e9e35dad98f6fc925001393a0beee42fb08e
Jorge Miguel Mendoza Correa <jorge.mendoza@bdcloud.onmicrosoft.com>
2024-11-08 13:46:58 -0600

    Add sumCommand

---
+++ Commads/SumCommand.cs
@@ -0,0 +1,12 @@
+﻿using COLORS.Commads.Interface;
+using COLORS.Models;
+
+namespace COLORS.Commads;
+
+public class SumCommand : IColorCommand
+{
+    public ContainerValue Execute(List<ContainerValue> instruction)
+    {
+        return new ContainerValue(Helpers.CommandsHelper.TryGetColor(Literals.Color.Red), instruction.Sum(i => (decimal)i.Value));
+    }
+}
