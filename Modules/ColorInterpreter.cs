4b98c1438f121894c3601f9465afff707c087577
Jorge Miguel Mendoza Correa <jorge.mendoza@bdcloud.onmicrosoft.com>
2024-11-08 15:31:48 -0600

    refactor interpeter

--- Modules/ColorInterpreter.cs
+++ Modules/ColorInterpreter.cs
@@ -1,42 +1,24 @@
-﻿using COLORS.Commads;
-using COLORS.Commads.Interface;
+﻿using COLORS.Factory;
+using COLORS.Helpers;
 using COLORS.Models;
+using COLORS.Modules.Interface;
 
 namespace COLORS.Modules;
 
-internal class ColorInterpreter
+public class ColorInterpreter(IColorCommandFactory _commandFactory) : IInterpeter
 {
-    private readonly Dictionary<string, ColorValue> variables;
-    private readonly Dictionary<string, IColorCommand> commands;
-
-    public ColorInterpreter()
-    {
-        variables = [];
-        commands = new Dictionary<string, IColorCommand>
-            {
-                { "RED", new RedCommand() }
-            };
-    }
-
     public void Execute(List<ColorInstruction> instructions)
     {
         foreach (var instruction in instructions)
         {
-            if (commands.TryGetValue(instruction.Color, out var command))
-            {
-                var result = command.Execute(instruction.LeftOperand, instruction.RightOperand);
-                Console.WriteLine($"Resultado de {instruction.Color}: {result.Number}");
+            if (!string.IsNullOrEmpty(instruction.VariableName) && StoregeManager.Variables.TryGetValue(instruction.VariableName, out ContainerValue? value))
+                instruction.Operands.Add(value);
 
-                if (!string.IsNullOrEmpty(instruction.VariableName))
-                {
-                    variables[instruction.VariableName] = result;
-                }
-            }
+            var command = _commandFactory.CreateCommand(instruction.Color);
+            var result = command.Execute(instruction.Operands);
+
+            if (result is { })
+                Console.WriteLine($"Resultado de {instruction.Color}: {result.Value}");
         }
-    }
-
-    public ColorValue GetVariable(string name)
-    {
-        return variables.TryGetValue(name, out var value) ? value : new ColorValue(0, "GRAY");
     }
 }
