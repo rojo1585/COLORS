e4d9168163bea87336d78b5c0b027b90869748b9
Jorge Miguel Mendoza Correa <jorge.mendoza@bdcloud.onmicrosoft.com>
2024-11-08 15:31:17 -0600

    rename class

--- Models/ColorValue.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace COLORS.Models;
-
-public record ColorValue(double number, string color)
-{
-    public double Number { get; set; } = number;
-    public string Color { get; set; } = color;
-}
---
+++ Models/ContainerValue.cs
@@ -0,0 +1,9 @@
+﻿
+namespace COLORS.Models;
+
+public record ContainerValue(string? Id, object Value, Type? Type = null)
+{
+    public Type? Type { get; set; } = Type;
+    public string? ID { get; set; } = Id;
+    public object Value { get; set; } = Value;
+}
