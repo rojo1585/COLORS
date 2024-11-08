82ba69c57c63026875b01d588df0a27414dfbfd3
Jorge Miguel Mendoza Correa <jorge.mendoza@bdcloud.onmicrosoft.com>
2024-11-08 12:32:04 -0600

    Add interface Comman

--- Commads/Interface/IColorCommand.cs
+++ Commads/Interface/IColorCommand.cs
@@ -4,5 +4,5 @@ namespace COLORS.Commads.Interface;
 
 public interface IColorCommand
 {
-    ColorValue Execute(ColorValue left, ColorValue right);
+    ContainerValue Execute(List<ContainerValue> instruction);
 }
